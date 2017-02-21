b8aa796b4dc2114d52a0a27e9965f24f659d60d8
Rob Kovtiuk <robsonheater@gmail.com>
2017-02-21 12:34:31 +0200

    Routes

--- src/main/js/route.js
+++ src/main/js/route.js
@@ -10,6 +10,32 @@
         return _.template($('#' + id).html());
     };
 
+    App.Router = Backbone.Router.extend({
+       routes: {
+           ''              : 'index',
+           'page/:id/*'    : 'page',
+           'search/:query' : 'search',
+           '*other'             : 'default'
+       },
 
+       index: function () {
+           console.log('index');
+       },
 
-});
+       page: function (id) {
+           console.log('Page on ' + id + '!!!');
+       },
+
+       search: function (query) {
+           console.log('Query: '+ query)
+       },
+
+       default: function () {
+           console.log('Page not fount')
+       }
+    });
+
+    new App.Router();
+    Backbone.history.start();
+
+}());
